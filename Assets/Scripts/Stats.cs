143d2d9444a2136d62e1bd6640bddff9bfebcc81
wiwiwcwc <40201289+wiwiwcwc@users.noreply.github.com>
2018-06-13 17:02:56 +0800

    0.021

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -18,27 +18,19 @@ public class Stats : MonoBehaviour {
 
 
 	void Start () {
-
-        attackdamage = card.attackdamage;
-        costText.text = card.cost.ToString();
-        nameText.text = card.name;
-        carddescriptionText.text = card.carddescription;
+        
 
     }
 	
 	// Update is called once per frame
 	void Update () {
-        /* var costdisplay = this.transform.Find("Cost");
+         var costdisplay = this.transform.Find("Cost");
          costdisplay.GetComponent<Text>().text = "" + cost;
          var title = this.transform.Find("Title");
          title.GetComponent<Text>().text = name;
          var description = this.transform.Find("Description");
          description.GetComponent<Text>().text = carddescription;
-         */
-        costText.text = card.cost.ToString();
-        nameText.text = card.name;
-        carddescriptionText.text = card.carddescription;
-
+         
 
 
 
